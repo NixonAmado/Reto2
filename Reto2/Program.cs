514258c43259fc185cb7aaa6d537e334a6f0ba39
Nixon Amado <86610615+NixonAmado@users.noreply.github.com>
2023-12-04 21:23:10 -0500

    Add files via upload

---
+++ Reto2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Net.Sockets;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // RETO 2
+        Console.Write(" Ingrese multiplicador: ");
+        int multiplicador = int.Parse(Console.ReadLine());
+        Console.Write(" Ingrese Multiplicando: ");
+        int multiplicando = int.Parse(Console.ReadLine());
+        Console.WriteLine("Resultdo: " + MultiplicacionRusa(multiplicando,multiplicador));
+    }
+
+    private static int MultiplicacionRusa (int mndo, int mdor)
+    {
+        List<int> multiplicandos = [mndo];
+        int result = 0;
+        do
+        {
+            if (mdor % 2 != 0)
+            {
+                result += mndo;
+            }
+            mndo *= 2;
+            mdor /= 2;
+        } while (mdor != 1);
+        result += mndo;
+        return result;
+    }
+}
